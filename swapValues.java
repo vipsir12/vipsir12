04d5f33fd4fb6d9025a300a8fbffaf84c1d48fdd
Vipin Gautam <115401117+vipsir12@users.noreply.github.com>
2023-01-27 17:44:34 +0530

    Add files via upload

---
+++ swapValues.java
@@ -0,0 +1,32 @@
+package function;
+
+public class swapValues {
+    public static void main (String[] args) {
+        //in java there is no pass by  reference only pass by value
+
+// value swapping for integers
+
+//        int a = 10;
+//        int b = 20;
+//        System.out.println(b); // value of b will be 20 here
+//        int temp =a;
+//        a = b;
+//        b = temp;
+//        //value of b will be swapped here , it will be 10
+//        System.out.println(b);
+
+// value swaping for the strings
+
+//      String naam = "vipin kumar";
+//      nameChange(naam);
+//        System.out.println(naam);
+//        //it will not change the naam because name has created the new variable not changed the previous one
+//
+//        //System.out.println(name);
+//        //we can't access the value of name outside the block
+//    }
+//    static void nameChange (String name ){
+//        name = "chandan kumar";
+
+    }
+}
